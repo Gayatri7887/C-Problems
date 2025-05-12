d505a3dff398342c4aba40c7f8e18cbb5eeaae99
Gayatri7887 <gayatriiiiii0512@gmail.com>
2025-05-12 19:31:30 +0530

    chapter3 : question 4 edited

--- Chapter 3/Question4.c
+++ Chapter 3/Question4.c
@@ -8,21 +8,22 @@ int main(){
 
     int day=1;
     
-    for (int i=1;i<=year;i++){
-    if((year%4==0)&&(year%100!=0)||(year%400==0)){
+    for (int i=1;i<year;i++){
+    if((i%4==0)&&(i%100!=0)||(i%400==0)){
         day=(day+2)%7;
     }
     else{
         day=(day+1)%7;
     }
-}
+    }
+
     if (day==1) printf("Monday\n");
     else if (day==2) printf("Tuesday\n");
     else if (day==3) printf("Wednesday\n");
-    else if (day==4) printf("Thrusday\n");
+    else if (day==4) printf("Thursday\n");
     else if (day==5) printf("Friday\n");
     else if (day==6) printf("Saturday\n");
-    else if (day==7) printf("Sunday\n");
+    else if (day==0) printf("Sunday\n");
 
     return 0;
 }
