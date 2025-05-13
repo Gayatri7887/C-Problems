a792148f60c51c5eea3b880c8c277dacfb2da19f
Gayatri7887 <gayatriiiiii0512@gmail.com>
2025-05-13 20:09:27 +0530

    chapter3 : question 8

---
+++ Chapter 3/Question8.c
@@ -0,0 +1,10 @@
+/*WAP to find the absolute value of the number entered through the keyboard.*/
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int b= abs(a);
+    printf("%d",b);
+    return 0;
+}
