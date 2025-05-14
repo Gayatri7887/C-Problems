f94369ff7d4789b1d2b064ae60a831e7e729f64d
Gayatri7887 <gayatriiiiii0512@gmail.com>
2025-05-14 15:11:57 +0530

    chapter 4 : question 9

---
+++ Chapter 4/Question8.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int day, month;
+
+    printf("Enter your birth month (1-12): ");
+    scanf("%d", &month);
+    
+    printf("Enter your birth day (1-31): ");
+    scanf("%d", &day);
+
+    if ((month == 1 && day >= 20) || (month == 2 && day <= 18))
+        printf("Your Zodiac sign is Aquarius");
+    else if ((month == 2 && day >= 19) || (month == 3 && day <= 20))
+        printf("Your Zodiac sign is Pisces");
+    else if ((month == 3 && day >= 21) || (month == 4 && day <= 19))
+        printf("Your Zodiac sign is Aries");
+    else if ((month == 4 && day >= 20) || (month == 5 && day <= 20))
+        printf("Your Zodiac sign is Taurus");
+    else if ((month == 5 && day >= 21) || (month == 6 && day <= 20))
+        printf("Your Zodiac sign is Gemini");
+    else if ((month == 6 && day >= 21) || (month == 7 && day <= 22))
+        printf("Your Zodiac sign is Cancer");
+    else if ((month == 7 && day >= 23) || (month == 8 && day <= 22))
+        printf("Your Zodiac sign is Leo");
+    else if ((month == 8 && day >= 23) || (month == 9 && day <= 22))
+        printf("Your Zodiac sign is Virgo");
+    else if ((month == 9 && day >= 23) || (month == 10 && day <= 22))
+        printf("Your Zodiac sign is Libra");
+    else if ((month == 10 && day >= 23) || (month == 11 && day <= 21))
+        printf("Your Zodiac sign is Scorpio");
+    else if ((month == 11 && day >= 22) || (month == 12 && day <= 21))
+        printf("Your Zodiac sign is Sagittarius");
+    else if ((month == 12 && day >= 22) || (month == 1 && day <= 19))
+        printf("Your Zodiac sign is Capricorn");
+    else
+        printf("Invalid date. Please check your input.");
+
+    return 0;
+}
---
+++ Chapter 4/Question9.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    float weight, height, BMI;
+    scanf("%f %f",&weight,&height);
+    BMI= weight/(height*height);
+    printf("BMI=%.2f\n",BMI);
+    if (BMI<11){
+        printf("Starvation");
+    }    
+    else if(BMI>=15.1 && BMI<=17.5){
+        printf("Anorexic");
+    }
+    else if(BMI>=17.6 && BMI<=18.5){
+        printf("Underweight");
+    }
+    else if (BMI>=18.6 && BMI<=24.9)
+    {
+        printf("Ideal");
+    }
+    else if(BMI>=25 && BMI<=25.9){
+        printf("Overweight");
+    }
+    else if(BMI>=17.6 && BMI<=18.5){
+        printf("Obese");
+    }
+    else{
+        printf("Morbidly Obese");
+    }
+    return 0;
+}
